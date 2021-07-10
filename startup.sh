017869aa2120807548649d777ee2af4b7f5acc6a
Mitsuaki KIDA <kidatti@gmail.com>
2021-07-11 01:16:26 +0900

    startup.sh .env、permission、key ganerata

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+cp .env.example .env
 rm -rf vendor
 rm -rf node_modules
-docker-compose build
+chmod 777 storage/*
+chmod 777 storage/framework/*
 docker-compose up -d
+docker-compose exec test-app-php php artisan key:generate
 docker-compose exec test-app-php composer install
 docker-compose exec test-app-php npm install

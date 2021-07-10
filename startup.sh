74f552eb706f30e4837897f348045d4de49b3750
Mitsuaki KIDA <kidatti@gmail.com>
2021-07-11 01:59:25 +0900

    startup.sh key generate 順番変更

--- startup.sh
+++ startup.sh
@@ -6,6 +6,6 @@ sudo rm -rf node_modules
 chmod 777 storage/*
 chmod 777 storage/framework/*
 docker-compose up -d
-docker-compose exec test-app-php php artisan key:generate
 docker-compose exec test-app-php composer install
 docker-compose exec test-app-php npm install
+docker-compose exec test-app-php php artisan key:generate
